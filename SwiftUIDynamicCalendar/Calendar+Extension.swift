ead380b227719d2b9482fb965fbb49b72e06fac4
Basel <basel.farag@gmail.com>
2023-08-02 17:03:34 -0500

    Update Calendar+Extension.swift
    To improve safety, avoid forced unwrapping (!). Instead, consider using optionals or default values.
    
    For lastDay(ofMonth:year:), instead of finding the first day of the next month and subtracting a day, I could directly set the day to 0 in the components for the next month, which gives the last day of the previous month.
    
    For addingMonth(), used the Calendar method date(byAdding:value:to:) which already handles this logic.
    
    Used constants for hard-coded values like 12 and 14 in the code for better readability.

--- SwiftUIDynamicCalendar/Calendar+Extension.swift
+++ SwiftUIDynamicCalendar/Calendar+Extension.swift
@@ -15,50 +15,43 @@ extension Calendar {
         return date.adding(days: -numberOfDaysAheadOfTheBeginningOfTheWeek)
     }
     
-    func firstDay(ofMonth month: Int, year: Int) -> Date {
+    func firstDay(ofMonth month: Int, year: Int) -> Date? {
         var components = DateComponents()
         components.month = month
         components.year = year
         components.day = 1
         components.hour = 12
         
-        return date(from: components)!
+        return date(from: components)
     }
     
-    func lastDay(ofMonth month: Int, year: Int) -> Date {
+    func lastDay(ofMonth month: Int, year: Int) -> Date? {
         var components = DateComponents()
-        components.month = month
-        components.year = year
-        components.day = 1
+        components.month = month == 12 ? 1 : month + 1
+        components.year = month == 12 ? year + 1 : year
+        components.day = 0
         components.hour = 12
         
-        let newComponents = components.addingMonth()!
-        return date(from: newComponents)!.adding(days: -1)
+        return date(from: components)
     }
     
-    func numberOfWeeks(inMonth month: Int, year: Int) -> Int {
-        let date = lastDay(ofMonth: month, year: year)
-        let weekRange = range(of: .weekOfMonth,
-                              in: .month,
-                              for: date)
-        return weekRange!.count
+    func numberOfWeeks(inMonth month: Int, year: Int) -> Int? {
+        guard let date = lastDay(ofMonth: month, year: year) else { return nil }
+        let weekRange = range(of: .weekOfMonth, in: .month, for: date)
+        return weekRange?.count
     }
     
     
     func isDate(_ dateA: Date, theSameAs dateB: Date, inComponents components: Set<Calendar.Component>) -> Bool {
         return dateComponents(components, from: dateA) == dateComponents(components, from: dateB)
     }
-    
 }
 
 extension TimeInterval {
-    
     static let hoursInADay: TimeInterval = 24
     static let secondsInAnHour: TimeInterval = 3600
     static let secondsInADay: TimeInterval = 24 * 3600
-    
 }
-
 
 extension Date {
     
@@ -66,33 +59,7 @@ extension Date {
         return addingTimeInterval(.secondsInADay * TimeInterval(days))
     }
     
-}
-
-extension DateComponents {
-    
-    func addingMonth() -> DateComponents? {
-        guard let month = month, let year = year else {
-            return nil
-        }
-        
-        var newComponents = self
-        
-        if month < 12 {
-            newComponents.month = month + 1
-        } else {
-            newComponents.year = year + 1
-            newComponents.month = 1
-        }
-        
-        return newComponents
-    }
-    
-}
-
-
-extension Date {
-    
-    static func date(day: Int, month:  Int, year: Int, hour: Int = 14) -> Date {
+    static func date(day: Int, month:  Int, year: Int, hour: Int = 14) -> Date? {
         var components = DateComponents()
         
         components.day = day
@@ -100,7 +67,7 @@ extension Date {
         components.year = year
         components.hour = hour
         
-        return Calendar.current.date(from: components)!
+        return Calendar.current.date(from: components)
     }
     
 }
